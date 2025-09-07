2e2cd755410a42e9ebbb8adc4000deac413cd7af
Madan <madanmohan14072002@gmail.com>
2025-09-07 14:03:36 +0530

    chore: initialize project with Serverless framework and Python dependencies
    - Added package.json for Node.js dependencies and scripts for formatting and deployment.
    - Created pyproject.toml for Python code formatting with Black.
    - Removed Python AWS CDK stack implementation as it is no longer needed.
    - Updated requirements.txt with necessary Python packages and removed requirements-dev.txt.
    - Added serverless.yml configuration for deploying the service on AWS.
    - Deleted unused source.bat and test files.

---
+++ handler.py
@@ -0,0 +1,24 @@
+import json
+
+
+def hello(event, context):
+    name = event.get("queryStringParameters", {}).get("name", "stranger")
+    body = {
+        "message": "Hello, {}. This is your Python Lambda function executed by Serverless Framework!".format(
+            name
+        ),
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
